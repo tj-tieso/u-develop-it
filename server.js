813f415d60293b7480c573f7483670567166c224
tj-tieso <tj.tieso@gmail.com>
2022-06-05 15:08:43 -0500

    can add candidate

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const inputCheck = require("./utils/inputCheck");
 const mysql = require("mysql2");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,6 +7,7 @@ const app = express();
 //middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// connect to db
 const db = mysql.createConnection(
   {
     host: "localhost",
@@ -73,17 +74,33 @@ app.delete("/api/candidate/:id", (req, res) => {
 });
 
 // create a new candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//             VALUES (?,?,?,?)`;
+app.post("/api/candidate", ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    "first_name",
+    "last_name",
+    "industry_connected"
+  );
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
 
-// const params = [1, "Ronald", "Firbank", 1];
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+    VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
 
-// db.query(sql, params, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// });
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: body,
+    });
+  });
+});
 
 // route to handle user requests that aren't supported by the app
 app.use((req, res) => {
