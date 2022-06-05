a6f047018f95103bc23fac491a96cdb550b1f35c
tj-tieso <tj.tieso@gmail.com>
2022-06-05 13:53:30 -0500

    databse call

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require("express");
+const mysql = require("mysql2");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 //middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "PoC31Lark)",
+    database: "election",
+  },
+  console.log("Connected to the election database.")
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 
 // route to handle user requests that aren't supported by the app
 app.use((req, res) => {
@@ -13,5 +27,5 @@ app.use((req, res) => {
 
 // hey, listen
 app.listen(PORT, () => {
-  console.log(`Hey! Listen! on port ${PORT}`);
+  console.log(`Hey! Listen! on http://localhost:${PORT}`);
 });
