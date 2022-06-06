09d5855048404a18b16c089c6dba8f5d9dc9dfc1
tj-tieso <tj.tieso@gmail.com>
2022-06-06 13:20:58 -0500

    update candidates party

--- server.js
+++ server.js
@@ -38,6 +38,8 @@ app.get("/api/candidates", (req, res) => {
   });
 });
 
+// START CANDIDATE ROUTES
+
 // request single candidate
 app.get("/api/candidate/:id", (req, res) => {
   const sql = `SELECT candidates.*, parties.name 
@@ -110,6 +112,93 @@ app.post("/api/candidate", ({ body }, res) => {
     });
   });
 });
+
+// change candidate party
+app.put("/api/candidate/:id", (req, res) => {
+  const errors = inputCheck(req.body, "party_id");
+
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+
+  const sql = `UPDATE candidates SET party_id = ?
+              WHERE id = ?`;
+  const params = [req.body.party_id, req.params.id];
+
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+    } else if (!result.affectedRows) {
+      res.json({ message: "Candidate not found" });
+    } else {
+      res.json({
+        message: "success",
+        data: req.body,
+        changes: result.affectedRows,
+      });
+    }
+  });
+});
+
+// END CANDIDATE ROUTES
+
+// START PARTY ROUTES
+
+// get all parties
+app.get("/api/parties", (req, res) => {
+  const sql = `SELECT * FROM parties`;
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
+
+// get single party id
+app.get("/api/party/:id", (req, res) => {
+  const sql = `SELECT * FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: row,
+    });
+  });
+});
+
+// delete party
+app.delete("/api/party/:id", (req, res) => {
+  const sql = `DELETE FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      // checks if anything was deleted
+    } else if (!result.affectedRows) {
+      res.json({
+        message: "Party not found",
+      });
+    } else {
+      res.json({
+        message: "deleted",
+        changes: result.affectedRows,
+        id: req.params.id,
+      });
+    }
+  });
+});
+
+// END PARTY ROUTES
 
 // route to handle user requests that aren't supported by the app
 app.use((req, res) => {
