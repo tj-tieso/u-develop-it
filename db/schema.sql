9578529a2acdc9e0e8d4296ab1f30178a849665a
tj-tieso <tj.tieso@gmail.com>
2022-06-07 11:15:36 -0500

    voter table set up

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS votes;
 -- candidates must be dropped before parties due to the foreign key constraint that requires the parties table to exist
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
@@ -25,4 +26,16 @@ CREATE TABLE voters (
   last_name VARCHAR(30) NOT NULL,
   email VARCHAR(50) NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE votes (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  voter_id INTEGER NOT NULL,
+  candidate_id INTEGER NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  -- signifies that the values inserted into the voter_id field must be unique.
+  CONSTRAINT uc_voter UNIQUE (voter_id),
+  -- ON DELETE CASCADE: deleting the reference key will also delete the entire row from this table
+  CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+  CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
 );
