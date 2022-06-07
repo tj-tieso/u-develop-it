cc735fa3c9f26329b6b6c845e34d878995cea137
tj-tieso <tj.tieso@gmail.com>
2022-06-07 13:53:26 -0500

    get all votes

--- routes/apiRoutes/voteRoutes.js
+++ routes/apiRoutes/voteRoutes.js
@@ -3,9 +3,27 @@ const router = express.Router();
 const db = require("../../db/connection");
 const inputCheck = require("../../utils/inputCheck");
 
-router.post('/vote', ({ body }, res) => {
+router.get("/votes", (req, res) => {
+  const sql = `SELECT candidates.*, parties.name AS party_name, COUNT(candidate_id) AS count
+                FROM votes
+                LEFT JOIN candidates ON votes.candidate_id = candidates.id
+                LEFT JOIN parties ON candidates.party_id = parties.id
+                GROUP BY candidate_id ORDER BY count DESC;`;
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: rows,
+    });
+  });
+});
+
+router.post("/vote", ({ body }, res) => {
   // Data validation
-  const errors = inputCheck(body, 'voter_id', 'candidate_id');
+  const errors = inputCheck(body, "voter_id", "candidate_id");
   if (errors) {
     res.status(400).json({ error: errors });
     return;
@@ -20,9 +38,9 @@ router.post('/vote', ({ body }, res) => {
       return;
     }
     res.json({
-      message: 'success',
+      message: "success",
       data: body,
-      changes: result.affectedRows
+      changes: result.affectedRows,
     });
   });
 });
